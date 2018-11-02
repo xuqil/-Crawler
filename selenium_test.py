83b1585d57051dda4025d9cd26b2b7240e89c397
许其龙 <13829842679@163.com>
2018-11-03 01:21:21 +0800

    开始selenium库的学习

--- selenium_test.py
+++ selenium_test.py
@@ -5,3 +5,15 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
 browser = webdriver.Chrome()
+try:
+    browser.get('https://www.baidu.com')
+    input = browser.find_element_by_id('kw')
+    input.send_keys('Python')
+    input.send_keys(Keys.ENTER)
+    wait = WebDriverWait(browser, 10)
+    wait.until(EC.presence_of_element_located((By.ID, 'content_left')))
+    print(browser.current_url)
+    print(browser.get_cookies())
+    print(browser.page_source)
+finally:
+    browser.close()
