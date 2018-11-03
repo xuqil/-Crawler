e66f444e9818257a4cb2d65d5a280d3d6c09fb3a
许其龙 <13829842679@163.com>
2018-11-03 11:34:37 +0800

    获取文本值

--- selenuim_test/test2.py
+++ selenuim_test/test2.py
@@ -39,5 +39,12 @@ browser = webdriver.Chrome()
 
 url = 'https://www.zhihu.com/explore'
 browser.get(url)
-browser.execute_script('window.scrollTo(0,document.body.scrollHeight)')
-browser.execute_script('alert("To Bottom")')
+# browser.execute_script('window.scrollTo(0,document.body.scrollHeight)')
+# browser.execute_script('alert("To Bottom")')
+# 获取元素
+# logo = browser.find_element_by_id('zh-top-link-logo')
+# print(logo)
+# print(logo.get_attribute('class'))
+# 获取文本值
+input = browser.find_element_by_class_name('zu-top-add-question')
+print(input.text)
