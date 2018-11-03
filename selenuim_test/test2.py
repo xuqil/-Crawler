e90a84d0d31227bce47401aaac60c215f9da9c0a
许其龙 <13829842679@163.com>
2018-11-03 11:01:19 +0800

    获取多个元素

--- selenuim_test/test2.py
+++ selenuim_test/test2.py
@@ -2,8 +2,17 @@ from selenium import webdriver
 
 browser = webdriver.Chrome()
 browser.get('https:www.taobao.com')
-input_first = browser.find_element_by_id('q')
-input_second = browser.find_element_by_css_selector('#q')
-# input_third = browser.find_element_by_xpath('//*[@id="q"')
-print(input_first, input_second)
+
+# 获取单个元素
+# input_first = browser.find_element_by_id('q')
+# input_second = browser.find_element_by_css_selector('#q')
+# # input_third = browser.find_element_by_xpath('//*[@id="q"')
+# print(input_first, input_second)
+# browser.close()
+
+# 获取多个元素
+lis = browser.find_elements_by_css_selector('.service-bd li')
+print(lis)
+for li in lis:
+    print(li)
 browser.close()
