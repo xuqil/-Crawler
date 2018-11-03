a581de78d8ad69f1c360d17d4f09d497abd5a351
许其龙 <13829842679@163.com>
2018-11-03 11:09:37 +0800

    元素交互操作

--- selenuim_test/test2.py
+++ selenuim_test/test2.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+import time
 
 browser = webdriver.Chrome()
 browser.get('https:www.taobao.com')
@@ -11,8 +12,17 @@ browser.get('https:www.taobao.com')
 # browser.close()
 
 # 获取多个元素
-lis = browser.find_elements_by_css_selector('.service-bd li')
-print(lis)
-for li in lis:
-    print(li)
-browser.close()
+# lis = browser.find_elements_by_css_selector('.service-bd li')
+# print(lis)
+# for li in lis:
+#     print(li)
+# browser.close()
+
+#元素交互操作
+input = browser.find_element_by_id('q')
+input.send_keys('iPhone')
+time.sleep(1)
+input.clear()
+input.send_keys('iPad')
+button = browser.find_element_by_class_name('btn-search')
+button.click()
