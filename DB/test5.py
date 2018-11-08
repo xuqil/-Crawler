4106820b943b820e3b2d3a0f606beddd41828fe5
许其龙 <13829842679@163.com>
2018-11-09 00:51:32 +0800

    csv字典的写入

--- DB/test5.py
+++ DB/test5.py
@@ -1,8 +1,15 @@
 import csv
 
-with open('data.csv', 'w') as csvfile:
-    writer = csv.writer(csvfile)
+# with open('data1.csv', 'w') as csvfile:
+#     writer = csv.writer(csvfile, delimiter=' ')  # 改变分隔符
+#     writer.writerow(['id', 'name', 'age'])
+#     writer.writerow(['10004', 'Mile', '20'])
+#     writer.writerow(['10002', 'Bob', '21'])
+#     writer.writerow(['10004', 'Jordan', '21'])
+
+with open('data2.csv', 'w') as csvfile:
+    writer = csv.writer(csvfile, delimiter=' ')  # 改变分隔符
     writer.writerow(['id', 'name', 'age'])
-    writer.writerow(['10004', 'Mile', '20'])
-    writer.writerow(['10002', 'Bob', '21'])
-    writer.writerow(['10004', 'Jordan', '21'])
+    writer.writerows([['10004', 'Mile', '20'], ['10002', 'Bob', '21'], ['10004', 'Jordan', '21']])
+
+
---
+++ DB/test6.py
@@ -0,0 +1,9 @@
+import csv
+
+# 字典的写入
+with open('data.csv', 'w') as csvfile:
+    fieldnames = ['id', 'name', 'age']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
+    writer.writerow({'id': '1001', 'name': 'Mike', 'age': 20})
+    writer.writerow({'id': '1004', 'name': 'Dike', 'age': 20})
