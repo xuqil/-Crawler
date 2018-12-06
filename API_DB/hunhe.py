b7f47e10d4b0af5676c8d4bd618b9d2e7bc871af
xuqil <13829842679@163.com>
2018-12-06 20:21:07 +0800

    装饰器

--- API_DB/hunhe.py
+++ API_DB/hunhe.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine, Column
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.types import Integer
+from sqlalchemy.ext.hybrid import hybrid_property, hybrid_method
 Engine = create_engine('mysql+mysqlconnector://root:19218@127.0.0.1:3306/test', encoding='utf8')
 Base = declarative_base()
 
@@ -19,6 +20,14 @@ class Interval(Base):
     start = Column(Integer)
     end = Column(Integer)
 
+    @hybrid_property
+    def length(self):
+        return self.end - self.start
+
+    @hybrid_method
+    def bigger(self, i):
+        return self.length > i
+
 
 def init_db():
     Base.metadata.create_all(Engine)
@@ -30,12 +39,20 @@ def init_db():
 # session.add(Interval(start=0, end=100))
 # session.commit()
 
+#
+# # 实例行为
+# ins = session.query(Interval).first()
+# print(ins.end - ins.start)
+#
+#
+# # 类行为
+# ins = session.query(Interval).filter(Interval.end - Interval.start > 10).first()
+# print(ins.id)
 
-# 实例行为
-ins = session.query(Interval).first()
-print(ins.end - ins.start)
 
+# ins = session.query(Interval).filter(Interval.length > 10).first()
+ins = session.query(Interval).filter(Interval.bigger(10)).first()
 
-# 类行为
-ins = session.query(Interval).filter(Interval.end - Interval.start > 10).first()
 print(ins.id)
+print(ins.bigger(1))
+
